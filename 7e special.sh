bf72295c28f4a7164989c49fae498e00736564c6
nicorobin0508 <117674705+nicorobin0508@users.noreply.github.com>
2023-10-05 08:02:10 +0530

    Update 7e special.sh

--- 7e special.sh
+++ 7e special.sh
@@ -17,3 +17,22 @@ parameter list (individually)
 parameter list (as single list)
 total number of parameters 0
 process id 127
+
+
+#OR
+echo "current program is $0"
+
+# Initialize the counter i
+i=0
+
+for ARGS in "$@"  # Quote "$@" to handle arguments that may have spaces or special characters
+do
+    let i=i+1
+    echo "argument $i is $ARGS"
+done
+
+# Use '#' for commenting in shell scripts, not '//'
+echo "parameter list (individually) $@" # individual list shows (@)
+echo "parameter list (as single list) $*" # single list shows (*)
+echo "total number of parameters $#"
+echo "process ID $$"
